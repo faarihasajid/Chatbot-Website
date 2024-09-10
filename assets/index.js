a2ef54a5a102af3c9ced5aed41d55cb5d04e9a62
faarihasajid <faarihakhokhar@gmail.com>
2024-09-11 01:07:47 +0500

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,60 @@
+import day from './day.png'
+import AI from './AI.png'
+import Av1 from './Av1.png'
+import Avatars from './Avatars.png'
+import Av2 from './Av2.png'
+import Av3 from './Av3.png'
+import Av4 from './Av4.png'
+import Av5 from './Av5.png'
+import Av6 from './Av6.png'
+import Container1 from './Container1.png'
+import Container2 from './Container2.png'
+import costeff from './costeff.png'
+import iPhone from './iPhone.png'
+import lb1 from './lb1.png'
+import lb2 from './lb2.png'
+import lb3 from './lb3.png'
+import lb4 from './lb4.png'
+import lb5 from './lb5.png'
+import MSupport from './MSupport.png'
+import seamless from './seamless.png'
+import Secure from './Secure.png'
+import aifront from './aifront.png'
+import imagew from './imagew.png'
+import image from './image.png'
+import rob from './rob.png'
+import exp from './exp.png'
+import aihands from './aihands.png'
+import aihands2 from './aihands2.png'
+
+export {
+    day,
+    aihands,
+    aihands2,
+    rob,
+    image,
+    imagew,
+    AI,
+    aifront,
+    Av1,
+    Av2,
+    Av3,
+    Av4,
+    Av5,
+    Av6,
+    exp,
+    Avatars,
+    Container1,
+    Container2,
+    costeff,
+    iPhone,
+    lb1,
+    lb2,
+    lb3,
+    lb4,
+    lb5,
+    MSupport,
+    seamless,
+    Secure,
+
+}
